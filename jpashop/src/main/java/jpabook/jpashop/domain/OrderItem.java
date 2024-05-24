7ed32d87a4a21622dfa913d496acb2500a35e869
killerwhale1125 <killerwhale1125@naver.com>
2024-05-24 15:52:08 +0900

    jpa shop add

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name="order_item")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;
+    private int count;
+}
