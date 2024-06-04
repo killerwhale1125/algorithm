61dcc85efbbd967bcf7f13e29a289a419ab1731b
killerwhale1125 <killerwhale1125@naver.com>
2024-06-04 11:04:49 +0900

    external properties2 commit

--- start/external-read/src/main/java/hello/config/MyDataSourceConfigV2.java
+++ start/external-read/src/main/java/hello/config/MyDataSourceConfigV2.java
@@ -2,16 +2,18 @@ package hello.config;
 
 import hello.datasource.MyDataSource;
 import hello.datasource.MyDataSourcePropertiesV1;
+import hello.datasource.MyDataSourcePropertiesV2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @Slf4j
-//@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
-public class MyDataSourceConfigV1 {
+@EnableConfigurationProperties(MyDataSourcePropertiesV2.class)
+public class MyDataSourceConfigV2 {
 
-    private final MyDataSourcePropertiesV1 properties;
+    private final MyDataSourcePropertiesV2 properties;
 
-    public MyDataSourceConfigV1(MyDataSourcePropertiesV1 properties) {
+    public MyDataSourceConfigV2(MyDataSourcePropertiesV2 properties) {
         this.properties = properties;
     }
 
