438ad960184576075a34f91617796b98656a15b9
killerwhale1125 <killerwhale1125@naver.com>
2024-06-08 10:39:33 +0900

    commit

---
+++ algorithm/src/brute_force/_1748_수이어쓰기1/Retry.java
@@ -0,0 +1,16 @@
+package brute_force._1748_수이어쓰기1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Retry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+
+        for(int i=1; i<=N.length(); i++) {
+
+        }
+    }
+}
