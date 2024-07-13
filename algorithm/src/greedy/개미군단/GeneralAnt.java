dedeed4f8791d2749b2a76d6ecaa6601fac3a171
killerwhale1125 <killerwhale1125@naver.com>
2024-07-13 17:45:45 +0900

    WIP: Commit local changes before revert

--- algorithm/src/greedy/개미군단/GeneralAnt.java
+++
@@ -1,8 +0,0 @@
-package greedy.개미군단;
-
-public class GeneralAnt extends Ant {
-
-    public GeneralAnt(String type) {
-        super(5, type);
-    }
-}
--- algorithm/src/greedy/개미군단/SolidAnt.java
+++
@@ -1,8 +0,0 @@
-package greedy.개미군단;
-
-public class SolidAnt extends Ant {
-    public SolidAnt(String type) {
-        super(3, type);
-    }
-
-}
