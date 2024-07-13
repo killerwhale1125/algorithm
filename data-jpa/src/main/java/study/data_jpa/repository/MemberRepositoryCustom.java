5e4f1fc8a4ec992b135640899559f90c8138bc8b
killerwhale1125 <killerwhale1125@naver.com>
2024-07-13 17:40:07 +0900

    delete files

--- data-jpa/src/main/java/study/data_jpa/repository/MemberRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package study.data_jpa.repository;
-
-import study.data_jpa.entity.Member;
-
-import java.util.List;
-
-public interface MemberRepositoryCustom {
-    List<Member> findMemberCustom();
-}
