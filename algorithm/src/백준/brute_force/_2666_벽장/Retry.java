f2d3a13c80ec6b1ab545b0a459e5ef7e62d4fbd8
killerwhale1125 <killerwhale1125@naver.com>
2024-09-12 20:04:38 +0900

    완전탐색 벽장 문제 재시도

--- algorithm/src/백준/brute_force/_2666_벽장/Retry.java
+++ algorithm/src/백준/brute_force/_2666_벽장/Retry.java
@@ -3,10 +3,9 @@ package 백준.brute_force._2666_벽장;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Retry {
     static int[] useBoard;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,19 +24,13 @@ public class Main {
         System.out.println(moveBoard(0, emptyFirst, emptySecond));
     }
 
-    static int moveBoard(int cnt, int emptyFirst, int emptySecond) {
-
-        // 더이상 사용할 벽장이 없을 때는 0 반환
+    private static int moveBoard(int cnt, int emptyFirst, int emptySecond) {
         if(cnt == useBoard.length) return 0;
 
-        // 이동한 거리 절댓값
-        int abs1 = Math.abs(emptyFirst - useBoard[cnt]);
-        int abs2 = Math.abs(emptySecond - useBoard[cnt]);
-        
-        // 현재 사용했던 벽장이 다음 빈칸의 벽장으로 바뀐다
-        return Math.min(
-                abs1 + moveBoard(cnt + 1, emptySecond, useBoard[cnt]),
-                abs2 + moveBoard(cnt + 1, emptyFirst, useBoard[cnt])
-        );
+        int abs1 = Math.abs(useBoard[cnt] - emptyFirst);
+        int abs2 = Math.abs(useBoard[cnt] - emptySecond);
+
+        return Math.min(abs1 + moveBoard(cnt + 1, emptySecond, useBoard[cnt]),
+                abs2 + moveBoard(cnt + 1, emptyFirst, useBoard[cnt]));
     }
 }
