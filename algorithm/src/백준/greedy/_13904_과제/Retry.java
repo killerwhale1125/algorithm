de4991440b89a9dbda528a804da688dbacccd779
killerwhale1125 <killerwhale1125@naver.com>
2024-11-03 17:06:17 +0900

    백준 greedy 13904번 과제 문제 기존 -> List 사용하여 remove에서 O(N) 시간복잡도 비효율적 재시도 -> Map과 우선순위 큐를 사용하여 O(nlogn)으로 시간복잡도 개선 및 최적화

--- algorithm/src/백준/greedy/_13904_과제/Retry.java
+++ algorithm/src/백준/greedy/_13904_과제/Retry.java
@@ -1,7 +1,44 @@
 package 백준.greedy._13904_과제;
 
-public class Retry {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class Retry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        StringTokenizer st;
+        int maxDay = Integer.MIN_VALUE;
+        // 1,000
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int day = Integer.parseInt(st.nextToken());
+            int score = Integer.parseInt(st.nextToken());
+
+            List<Integer> list = map.computeIfAbsent(day, k -> new ArrayList<>());
+            list.add(score);
+            maxDay = Math.max(maxDay, day);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int sum = 0;
+        // 1,000
+        for(int i=maxDay; i>=1; i--) {
+            List<Integer> list = map.get(i);
+            if(list != null && !list.isEmpty()) {
+                for(int x : list) {
+                    // O(nlogn) -> O( 1000 * log1000 )
+                    pq.add(x);
+                }
+            }
+
+            if(!pq.isEmpty()) {
+                sum += pq.poll();
+            }
+        }
+
+        System.out.println(sum);
     }
 }
